d9bdb824c1603aba2e97e8bca271dfb1e1def672
Patrick Fischer <patrickfischer@MacBook-Pro-1546.local>
2026-04-05 17:01:31 +0200

    Configure Supabase project credentials

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.SUPABASE_CONFIG = {
-  url: "",
-  anonKey: "",
+  url: "https://zwalghbihdaznahqllap.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3YWxnaGJpaGRhem5haHFsbGFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzNzI5NzcsImV4cCI6MjA5MDk0ODk3N30.FUfNA67YSdXGhn6hgl3lXzmG9iaPDmAnwvB-ciPaQiY",
   courseId: "geschichte_bis_1500",
   projectLabel: "Geschichte bis 1500"
 };
